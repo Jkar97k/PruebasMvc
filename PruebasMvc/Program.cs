39b27e08a36fd8fdb359e8ac13f550282f34ab6a
JK <colegioret@hotmail.com>
2024-05-20 21:58:56 -0500

    bb

--- PruebasMvc/Program.cs
+++ PruebasMvc/Program.cs
@@ -77,6 +77,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Usuario}/{action=Index}/{id?}");
 
 app.Run();
